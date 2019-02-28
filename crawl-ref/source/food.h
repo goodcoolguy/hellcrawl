beee3f260ef956acddbf22a2291f265d29c66e5f
hellmonk <nldehaan91@gmail.com>
2019-02-27 20:53:13 -0500

    makefile changes

--- crawl-ref/source/food.h
+++
@@ -1,9 +0,0 @@
-/**
- * @file
- * @brief Functions for eating and butchering.
-**/
-
-#ifndef FOOD_H
-#define FOOD_H
-
-#endif
--- crawl-ref/source/l_food.cc
+++
@@ -1,9 +0,0 @@
-#include "AppHdr.h"
-
-#include "l_libs.h"
-
-#include "butcher.h"
-#include "cluautil.h"
-#include "food.h"
-#include "itemprop.h"
-#include "player.h"
